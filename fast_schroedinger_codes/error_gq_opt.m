2f5b5b53ffe2a50d1ebebfcf896f02884d5e97ff
leki <lehelb@yahoo.com>
2019-11-28 15:51:28 +0000

    moved warning on choice of parameters for 3stage method

--- fast_schroedinger_codes/error_gq_opt.m
+++ fast_schroedinger_codes/error_gq_opt.m
@@ -3,11 +3,7 @@ function f = error_gq_opt(Q,B,Ljm1,d,RK,dt,n0)
 if (RK == 1) %BDF1     
     rn = @(z) 1./abs(1-z); qn = @(z) 1./abs(1-z);
 else %currently just 2-stage Radau       
-    rn = @(z) abs((2*z+6)./(z.^2-4*z+6)); qn = @(z) sqrt((9/2)^2+abs(3/2-z).^2)./abs(z.^2-4*z+6); 
-    if (RK > 2)
-        disp(['Choice of parameters for 2-stage Radau IIA used. Might not be ' ...
-              'optimal for the 3-stage version']);
-    end
+    rn = @(z) abs((2*z+6)./(z.^2-4*z+6)); qn = @(z) sqrt((9/2)^2+abs(3/2-z).^2)./abs(z.^2-4*z+6);     
 end
 
 M1 = 20; M2 = 20;
