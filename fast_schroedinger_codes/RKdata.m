5d537b7e2bf2ab42a6d677bad25aa246e3457fd7
leki <lehelb@yahoo.com>
2019-11-29 08:25:35 +0000

    remove RK flag

--- fast_schroedinger_codes/RKdata.m
+++ fast_schroedinger_codes/RKdata.m
@@ -1,5 +1,5 @@
-function [A,b,c,intflag] = RKdata(RK)
-%function [A,b,c,intflag] = RKdata(RK)
+function [A,b,c] = RKdata(RK)
+%function [A,b,c] = RKdata(RK)
 %returns coefficients in Butcher notation
 %RK = 0 for backward Euler
 %RK = 1 for 2 stage Radau IIA
@@ -7,22 +7,18 @@ function [A,b,c,intflag] = RKdata(RK)
 
 if (RK == 1)
     %BDF1
-    A = 1; c = 1; b = 1;
-        intflag ='RadauIIA1';    
+    A = 1; c = 1; b = 1;        
 elseif (RK==2)
     %radau IIa of order 3 stage order 2
     A = [5/12 -1/12; 3/4 1/4]; c = [1/3; 1]; 
-    b = [3/4; 1/4];
-        intflag ='RadauIIA3';    
+    b = [3/4; 1/4];        
 elseif (RK==3)
 %radau IIa order 5, stage order 3
     A = [(88-7*sqrt(6))/360 (296-169*sqrt(6))/1800 (-2+3*sqrt(6))/225;
          (296+169*sqrt(6))/1800 (88+7*sqrt(6))/360 (-2-3*sqrt(6))/225;
         (16-sqrt(6))/36 (16+sqrt(6))/36 1/9];
     b = [(16-sqrt(6))/36 (16+sqrt(6))/36 1/9].';
-    c = [(4-sqrt(6))/10 ; (4+sqrt(6))/10 ; 1];
-        intflag ='RadauIIA5';    
+    c = [(4-sqrt(6))/10 ; (4+sqrt(6))/10 ; 1];        
 else
-    fprintf('only RK types 1,2,3 possible, see help\n');
-    return;
+    error('only RK types 1,2,3 possible, see help\n');    
 end
